09b94f38be33d57a6d096ee2d543595107f2ae65
SharpSight44 <Leoest011@gmail.com>
2022-10-05 05:24:22 +0000

    pic

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './cdf.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={{ color: '#403534', fontSize: 18, backgroundColor: 'yellow' }}>We have it working Kim! Yay</Text>
+      <Image source={logo} style={{ width: 200, height: 200, marginTop: 30}} />
       <StatusBar style="auto" />
     </View>
   );
